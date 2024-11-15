29b3d5aaaa92eeb25e52719b33cc483cde8db606
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2024-11-15 20:35:01 +0530

    Add files via upload

---
+++ list.py
@@ -0,0 +1,11 @@
+# list are nothing but 
+tea_types=["lemon","wolong"]
+print(tea_types)
+print(tea_types[1])
+# if we want to change the values let see
+tea_types[1]="hurbal"
+print(tea_types)
+#note it will start from the 0 th position only
+# if we wanted to add more also we can add example
+tea_types[1:]=["black","lemon"]
+print(tea_types)
