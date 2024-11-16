ca1ee8f19ba8359207deb1814faf4d0e26a45309
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2024-11-16 21:04:28 +0530

    Add files via upload

---
+++ learn DSA/evenodd.py
@@ -0,0 +1,6 @@
+# check whether the given  number is even or odd
+num=int(input("enter the number to chech even or odd:-\n"))
+if num%2==0:
+    print(f'the given number {num} is even')
+else:
+     print(f'the given number {num} is odd')
---
+++ learn DSA/leapornot.py
@@ -0,0 +1,6 @@
+# write a program check whether the leap year or not
+year=int(input("enter the year"))
+if year%4==0:
+    print("leap")
+else:
+    print("not")
