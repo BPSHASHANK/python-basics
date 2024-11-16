0409aa3600fef2c7d547dd2c7a9f177c126e8946
B P SHASHANK <154719115+BPSHASHANK@users.noreply.github.com>
2024-11-16 21:02:30 +0530

    Add files via upload

---
+++ primeornot.py
@@ -0,0 +1,6 @@
+# write a program to check given number is prime or not
+n=int(input("enter the number to check prime or not \n"))
+if n%2==0:
+    print(f'the given number {n} is prime number')
+else:
+    print(f'the given number {n} is not a prime number')
